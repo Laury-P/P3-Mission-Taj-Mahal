f45d9b82f8201a17061b20c3719769dbf45fb99f
Laury PRIN <prinlauryy@gmail.com>
2025-09-24 13:59:33 +0200

    Ajout des fichiers pour le fragment pour les avis: fichier java, view model et layout xml

---
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/NewReviewViewModel.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.tajmahal.ui.restaurant;
+
+import androidx.lifecycle.ViewModel;
+
+public class NewReviewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
